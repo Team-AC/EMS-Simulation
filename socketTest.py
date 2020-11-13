062af4aa27a522ca1fd7d6025496da2bcc9eb8f1
Sean Su <susean17@my.yorku.ca>
2020-11-12 19:21:16 -0500

    changed name

--- socketTest.py
+++ socketTest.py
@@ -7,11 +7,6 @@ def connect():
     print('connection established')
 
 @sio.event
-def my_message(data):
-    print('message received with ', data)
-    sio.emit('my response', {'response': 'my response'})
-
-@sio.event
 def disconnect():
     print('disconnected from server')
 
