19c3813897c4c0720b91a72c3f47dace9752d1a5
Basil Mahmood <basilmahmood98@gmail.com>
2020-12-20 17:24:33 -0500

    Modularized our code

--- simulation.py
+++ simulation.py
@@ -1,16 +1,9 @@
-from datetime import datetime, timedelta, time
-from time import sleep
-import threading
-import random
 import socketio
-import power_from_time_model
+from modules.murb_module.murb_simulation import murb_simulation_init 
+from modules.ev_module.ev_simulation import ev_simulation_init 
+from modules.bess_module.bess_simulation import bess_simulation_init 
 
 sio = socketio.Client()
-
-my_date = datetime.utcnow()
-
-counter = 0
-power = 0
 
 @sio.event
 def connect():
@@ -24,81 +17,10 @@ def connect_error():
 def disconnect():
     print("I'm disconnected!")
 
-dict_time_jump = {
-    "pastDay": (4*24)-1,
-    "pastWeek": (4*24*7)-1,
-    "pastMonth": (4*24*30)-1,
-    "pastYear": (4*24*365)-1
-}
-
-dict_time_delta = {
-    "pastDay": 1,
-    "pastWeek": 1*7,
-    "pastMonth": 1*30,
-    "pastYear": 1*365 
-}
-
-def realTimeData():
-    global timer
-    timer = threading.Timer(900.0, realTimeData)
-    timer.start()
-    TimeStamp = datetime.utcnow().isoformat()
-    Power = power_from_time_model.power_from_time(datetime.utcnow())
-    sio.emit('New Murb Power', {
-        'TimeStamp': TimeStamp,
-        'Power': Power
-    })
-
-@sio.on('Generate Murb Power')
-def send_past_day(interval):
-    global counter
-    global power
-    interval_start = datetime.utcnow()-timedelta(dict_time_delta[interval])
-
-    realTimeData()
-    while counter < (dict_time_jump[interval]):
-        counter = counter + 1
-        interval_start = interval_start + timedelta(0, 900)
-        TimeStamp = interval_start.isoformat()
-        Power = power_from_time_model.power_from_time(interval_start)
-
-        sio.emit('Old Murb Power', {
-            'TimeStamp': TimeStamp,
-            'Power': Power
-        })
-
-        # Sleep every 100th iteration to prevent server from getting overloaded
-        if ((counter % 100) == 1):
-            sleep(3)
-    
-    counter = 0
-
-@sio.on('Status Check')
-def status_check():
-    if 'timer' in globals():
-        real_time_data_status = timer.is_alive()
-    else:
-        real_time_data_status = False
-
-     
-    return { 
-        'real_time_data_status': real_time_data_status,
-        'data_generate_config': dict_time_jump
-    }
-
-
-@sio.on('Pre - Generate Murb Power')
-def pre_send_past_day():
-    print("Received")
-
-
-@sio.on('Stop Murb Power')
-def stop_murb_data():
-    global timer
-    if 'timer' in globals():
-        timer.cancel()
-
-data = []
+# Initialize the simulations
+murb_simulation_init(sio)
+ev_simulation_init(sio)
+bess_simulation_init(sio)
 
 sio.connect('http://localhost:3000')
 
