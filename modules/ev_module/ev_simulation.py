1b857c437e4323e5e0e02898b223d409478a6067
Nerushan Naguleswaran <neonerushan@gmail.com>
2020-12-26 21:15:40 -0500

    Ev save point

--- modules/ev_module/ev_simulation.py
+++ modules/ev_module/ev_simulation.py
@@ -1,5 +1,6 @@
 from datetime import datetime
 from threading import Timer
+import random
 
 def end_charging(charge_time, power):
     global sio
@@ -14,19 +15,51 @@ def start_charging(charge_time, power):
     Timer(charge_time, end_charging, (charge_time, power)).start()
 
 def check_ev():
-    ev_entered = True
-    charge_time = 15
-    power = 30
+    # user inputs: number of chargers,
+    
+    # ASSUMING ONE CHARGER RN
+    
+    # this the chance that ev comes in and wants to charge
+    probability_of_ev_entered = 0.05
+    probability_of_ev_charging = 0.85
+    chance_ev_wants_charge = random.uniform(0, 1)
 
-    if (ev_entered):
-        start_charging(charge_time, power)
+    if (chance_ev_wants_charge <= probability_of_ev_entered*probability_of_ev_charging):
+        ev_wanting_charge = True
+    else:
+        ev_wanting_charge = False
 
-def ev_simulation_init(sio_passed_in):
+    # ev chargers avalible (for assume only one charger for now) # what charger is avablie and go to
+    # charger in use or not
+    
+    # ev charge time and power consumed (to figure out)
+    # small/med/Large battery
+
+    probability_of_evbattery_small = 0.2
+    probability_of_evbattery_med = 0.3
+    probability_of_evbattery_large = 0.5
+    chance_ev_battery_size = random.uniform(0, 1)
+
+    if (ev_wanting_charge):
+
+        if (chance_ev_battery_size < 0.2):
+            charge_time = 10 # algo for time 
+            power = 5 # algo for power 
+            start_charging(charge_time, power)
+        elif  ((chance_ev_battery_size >= 0.2) and (chance_ev_battery_size < 0.5)):
+            charge_time = 20
+            power = 10
+            start_charging(charge_time, power)
+        else:
+            charge_time = 30
+            power = 15
+            start_charging(charge_time, power)
+
+def ev_simulation_init(sio_passed_in = None):
     global sio
     sio = sio_passed_in
 
     sio.sleep(1)
 
     check_ev()
-    Timer(30, ev_simulation_init).start()
-
+    Timer(5, ev_simulation_init).start()
