f59acae912f425ddc173575f6c0c4c9c456329e0
Basil Mahmood <basilmahmood98@gmail.com>
2021-01-09 17:54:59 -0500

    Fixed issue

--- modules/ev_module/ev_simulation.py
+++ modules/ev_module/ev_simulation.py
@@ -6,18 +6,6 @@ from modules.ev_module.real_time_data import real_time_data_start
 def ev_simulation_init(sio):
     sio.sleep(1)
 
-    real_time_data_start(sio)
-    
-    """ @sio.on('Generate Ev')
+    @sio.on('Generate Ev')
     def generate_ev(paramters_dict):
-        lvl_2 = [0 for x in range(int(paramters_dict["num_ev_level_2"])]
-        lvl_3 = [0 for x in range(int(paramters_dict["num_ev_level_3"])]
-        real_time_data(pramters_dict, sio) """
-
-    def generate_past_ev_data():
-        ev_current_time = datetime.utcnow()
-        ev_start_time_counter = ev_start_time = datetime.utcnow() - timedelta(hours=24) 
-        
-        
-            
-    generate_past_ev_data() 
+        real_time_data_start(sio)
