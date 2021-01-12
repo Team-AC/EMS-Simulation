b255ee423c68d5173299102c3a7618ffc3c2517a
Sean Su <susean17@my.yorku.ca>
2021-01-12 18:45:52 -0500

    Changed the probability

--- modules/ev_module/ev_chargetime_and_power.py
+++ modules/ev_module/ev_chargetime_and_power.py
@@ -6,17 +6,14 @@ import numpy as np
 
 
 def ev_chargetime_and_power(ev_wanting_charge,ev_battery_start_percentage, ev_charger_level, ev_charger_num):
-    probability_of_evbattery_small = 0.2 # MIGHT CHANGE FOR ACCURACY
-    probability_of_evbattery_med = 0.3
-    probability_of_evbattery_large = 0.5
     chance_ev_battery_size = random.uniform(0, 1)
     
     if ev_wanting_charge == 1 and ev_charger_level == 2:
-        if chance_ev_battery_size < 0.2:
+        if chance_ev_battery_size < 0.2: # 20% of EV's are small
             min_charge_percentage = ev_battery_start_percentage + round(gauss(15,5))
             ev_battery_end_percentage = randrange(min_charge_percentage, 100, 1) # NEED TO CHANGE FOR BETTER ACCURACY
             ev_battery_small = randrange(17,49,1) #NEED TO FIND AVERAGE BATTERY CAPACITY FOR SIZE
-            ev_charger_lvl_2_rate = 7 #RATE for lvl 2
+            ev_charger_lvl_2_rate = 7 #RATE for lvl 2 level two 7.7- 22 kw
             
             #algo for power and time
             power = ((ev_battery_end_percentage - ev_battery_start_percentage)/100)*ev_battery_small
@@ -24,7 +21,7 @@ def ev_chargetime_and_power(ev_wanting_charge,ev_battery_start_percentage, ev_ch
             
             return charge_time, power, ev_charger_num, ev_charger_level
             
-        elif ((chance_ev_battery_size >= 0.2) and (chance_ev_battery_size < 0.5)):
+        elif ((chance_ev_battery_size >= 0.2) and (chance_ev_battery_size < 0.8)): # 60% of EV's are Medium size
             min_charge_percentage = ev_battery_start_percentage + round(gauss(15,5))
             ev_battery_end_percentage = randrange(min_charge_percentage,100,1) # this will be the leaving battery percentage 
             ev_battery_med = randrange(50,84,1) #NEED TO FIND AVERAGE BATTERY CAPACITY FOR SIZE
@@ -49,11 +46,11 @@ def ev_chargetime_and_power(ev_wanting_charge,ev_battery_start_percentage, ev_ch
     
     elif ev_wanting_charge == 1 and ev_charger_level == 3: # Level 3
        
-        if chance_ev_battery_size < 0.2:
+        if chance_ev_battery_size < 0.2: # 20% of EV's are small
             min_charge_percentage = ev_battery_start_percentage + round(gauss(15,5))
             ev_battery_end_percentage = randrange(min_charge_percentage,100,1)
             ev_battery_small = randrange(17,49,1) #NEED TO FIND AVERAGE BATTERY CAPACITY FOR SIZE
-            ev_charger_lvl_3_rate = 50 #RATE for lvl 3
+            ev_charger_lvl_3_rate = 50 #RATE for lvl 3 25 - 350 kw
             
             #algo for power and time
             power = ((ev_battery_end_percentage - ev_battery_start_percentage)/100)*ev_battery_small
@@ -61,7 +58,7 @@ def ev_chargetime_and_power(ev_wanting_charge,ev_battery_start_percentage, ev_ch
 
             return charge_time, power, ev_charger_num, ev_charger_level
             
-        elif ((chance_ev_battery_size >= 0.2) and (chance_ev_battery_size < 0.5)):
+        elif ((chance_ev_battery_size >= 0.2) and (chance_ev_battery_size < 0.8)): # 60% of EV's are Medium size
             min_charge_percentage = ev_battery_start_percentage + round(gauss(15,5))
             ev_battery_end_percentage = randrange(min_charge_percentage,100,1)
             ev_battery_med = randrange(50,84,1) #NEED TO FIND AVERAGE BATTERY CAPACITY FOR SIZE
@@ -76,7 +73,7 @@ def ev_chargetime_and_power(ev_wanting_charge,ev_battery_start_percentage, ev_ch
         else:
             min_charge_percentage = ev_battery_start_percentage + round(gauss(15,5))
             ev_battery_end_percentage = randrange(min_charge_percentage,100,1)
-            ev_battery_large = randrange(85,200,1) #NEED TO FIND AVERAGE BATTERY CAPACITY FOR SIZE
+            ev_battery_large = randrange(85,120,1) #NEED TO FIND AVERAGE BATTERY CAPACITY FOR SIZE
             ev_charger_lvl_3_rate = 50 #RATE for lvl 3
 
             #algo for power and time
