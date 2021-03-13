4c90304967bf02221e0d042c55f47d365900d384
Basil Mahmood <basilmahmood98@gmail.com>
2021-03-13 18:03:08 -0500

    Added internal_clock to energy_control

--- simulation_modules/bess_module/energy_control.py
+++ simulation_modules/bess_module/energy_control.py
@@ -25,39 +25,42 @@ def validate_schedule(schedule):
             "minute": 0
         },
         "end": {
-            "hour": 23,
-            "minute": 59
+            "hour": 12,
+            "minute": 0
         },
         "mode": "charge"
+    }, {
+        "start": {
+            "hour": 12,
+            "minute": 0
+        },
+        "end": {
+            "hour": 24,
+            "minute": 59
+        },
+        "mode": "discharge_ev"
     }]
 
     return temp_schedule
-
-def is_time_between(begin_time, end_time, check_time=None):
-# If check time is not given, default to current UTC time
-    check_time = check_time or datetime.utcnow().time()
-    if begin_time < end_time:
-        return check_time >= begin_time and check_time <= end_time
-    else: # crosses midnight
-        return check_time >= begin_time or check_time <= end_time
-
+    
 ### Class Definition ###
 class EnergyControl:
     def __init__(self, bess, schedule, sio):
         self.bess = bess
         self.schedule = validate_schedule(schedule)
         self.sio = sio
+        self.internal_clock = None
 
     def clock_update(self, new_datetime):
         bess = self.bess
-
+        self.internal_clock = new_datetime
         bess_status = bess.clock_update(new_datetime)
     
     def update_schedule(self, schedule):
         self.schedule = validate_schedule(schedule)
     
     def get_current_mode(self):
-        current_time = datetime.now(timezone.utc)
+        current_time = self.internal_clock
         for schedule_dict in self.schedule:
             if ((schedule_dict["start"]["hour"] <= current_time.hour) and (schedule_dict["start"]["minute"] <= current_time.minute) and (schedule_dict["end"]["hour"] >= current_time.hour) and (schedule_dict["end"]["minute"] >= current_time.minute)):
                 return schedule_dict["mode"]
