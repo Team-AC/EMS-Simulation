ff9024eaefeafcc35f72dba39712df912c36cd06
Hassan Bajwa <hassan.b404@gmail.com>
2021-03-29 21:14:20 -0400

    done

--- optimization_modules/predict_ev.py
+++ optimization_modules/predict_ev.py
@@ -14,116 +14,120 @@ def predict_ev_init(sio):
     @sio.on('Generate EV Prediction')
     def generate_ev_predict(historic_data, ev_predict_params, list_timestamps):
 
-        empty_list = []
-        if not historic_data[0]['aggregatedData'] or not historic_data[1]['aggregatedData'] or not historic_data[2]['aggregatedData']:
-            return empty_list
-
+    
+        # A search function is created to search for different time period keys
+        
         def search(interval):
             for p in historic_data:
                 if p['interval'] == interval:
                     return p
         
-        list_predict = []
-
-        for timestamp in list_timestamps:
-            list_predict.append({
-                'TimeStamp': timestamp,
-                'Power': 0
-            })
-
-        past_month_dictionary = search('pastMonth')
-        past_3_months_dictionary = search('past3Months')
-        past_year_dictionary = search('pastYear')
         
-        def get_value(listOfDicts, key):
-            for subVal in listOfDicts:
-                if key in subVal:
-                    return subVal[key]
-
-    
-
+        past_month_dictionary = search('pastMonth')
+        past_year_dictionary = search('pastYear')
+        past_week_hourly_dictionary = search('pastWeekHourly')
+        
         #Aggregated Data Segments
-        aggregated_data1 = historic_data[0]['aggregatedData']
-        aggregated_data2 = historic_data[1]['aggregatedData']
-        aggregated_data3 = historic_data[2]['aggregatedData']
+        past_month_aggregated_data = past_month_dictionary['aggregatedData']
+        past_year_aggregated_data = past_year_dictionary['aggregatedData']
+        past_week_hourly_aggregated_data = past_week_hourly_dictionary['aggregatedData']
 
         #Total Power Segments
-        total_power_segment1 = [sub['TotalPower'] for sub in aggregated_data1]
-        total_power_segment2 = [sub['TotalPower'] for sub in aggregated_data2]
-        total_power_segment3 = [sub['TotalPower'] for sub in aggregated_data3]
+        total_power_past_month = [sub['TotalPower'] for sub in past_month_aggregated_data]
+        total_power_past_year = [sub['TotalPower'] for sub in past_year_aggregated_data]
+        total_power_past_week_hourly = [sub['TotalPower'] for sub in past_week_hourly_aggregated_data]
 
         #EV Charger Type Segments
-        charger_type1 = [sub['EvChargerType'] for sub in aggregated_data1]
-        charger_type2 = [sub['EvChargerType'] for sub in aggregated_data2]
-        charger_type3 = [sub['EvChargerType'] for sub in aggregated_data3]
+        charger_type_past_month = [sub['EvChargerType'] for sub in past_month_aggregated_data]
+        charger_type_past_year = [sub['EvChargerType'] for sub in past_year_aggregated_data]
+        charger_type_past_week_hourly = [sub['EvChargerType'] for sub in past_week_hourly_aggregated_data]
 
         #Aggregated Amount Segments
-        aggregated_amount_segment1 = [sub['AggregatedAmount'] for sub in aggregated_data1]
-        aggregated_amount_segment2 = [sub['AggregatedAmount'] for sub in aggregated_data2]
-        aggregated_amount_segment3 = [sub['AggregatedAmount'] for sub in aggregated_data3]
+        aggregated_amount_past_month = [sub['AggregatedAmount'] for sub in past_month_aggregated_data]
+        aggregated_amount_past_year = [sub['AggregatedAmount'] for sub in past_year_aggregated_data]
+        aggregated_amount_past_week_hourly = [sub['AggregatedAmount'] for sub in past_week_hourly_aggregated_data]
 
         #Timestamp Segments
-        timestamp_segment_1 = [sub['TimeStamp'] for sub in aggregated_data1]
-        timestamp_segment_2 = [sub['TimeStamp'] for sub in aggregated_data2]
-        timestamp_segment_3 = [sub['TimeStamp'] for sub in aggregated_data3]
+        timestamps_past_month = [sub['TimeStamp'] for sub in past_month_aggregated_data]
+        timestamps_past_year = [sub['TimeStamp'] for sub in past_year_aggregated_data]
+        timestamps_past_week_hourly = [sub['TimeStamp'] for sub in past_week_hourly_aggregated_data]
 
-        #Hour Segments
-        day1 = []
-        day2 = []
-        day3 = []
+        #Empty lists segments of time (hours,days,months) as required
+
+        #Day Segments
+        day_past_month = []
+        day_past_year = []
+        day_past_week_hourly = []
 
         #Month Segments
-        month1 = []
-        month2 = []
-        month3 = []
+        month_past_month = []
+        month_past_year = []
+        month_past_week_hourly = []
 
-        #Hour Segment 1
+        #Hour Segments
+        hour_past_month = []
+        hour_past_year = []
+        hour_past_week_hourly = []
 
-        for value1 in timestamp_segment_1:
+        #Append segments of time (hours,days,months) past month
+
+        for value1 in timestamps_past_month:
             append_time_parameters = dateutil.parser.parse(value1)
-            day1.append(append_time_parameters.day)
-            month1.append(append_time_parameters.month)
+            day_past_month.append(append_time_parameters.day)
+            month_past_month.append(append_time_parameters.month)
+            hour_past_month.append(append_time_parameters.hour)
 
-        #Hour Segment 2
+        #Append segments of time (hours,days,months) past year
 
-        for value2 in timestamp_segment_2:
-            append_time_parameters2 = dateutil.parser.parse(value2)
-            day2.append(append_time_parameters2.day)
-            month2.append(append_time_parameters2.month)
-
-        #Hour Segment 3
-        for value3 in timestamp_segment_3:
+        for value3 in timestamps_past_year:
             append_time_parameters3 = dateutil.parser.parse(value3)
-            day3.append(append_time_parameters3.day)
-            month3.append(append_time_parameters3.month)
+            day_past_year.append(append_time_parameters3.day)
+            month_past_year.append(append_time_parameters3.month)
+            hour_past_year.append(append_time_parameters.hour)
 
-        #Combine all the lists
+        #Append segments of time (hours,days,months) past week hourly
 
-        combined_list_segment_1 = month1 + day1 + charger_type1 + aggregated_amount_segment1 + total_power_segment1
-        combined_list_segment_2 = month2 + day2 + charger_type2 + aggregated_amount_segment2 + total_power_segment2
-        combined_list_segment_3 = month3 + day3 + charger_type3 + aggregated_amount_segment3 + total_power_segment3
+        for value4 in timestamps_past_week_hourly:
+            append_time_parameters3 = dateutil.parser.parse(value4)
+            day_past_week_hourly.append(append_time_parameters3.day)
+            month_past_week_hourly.append(append_time_parameters3.month)
+            hour_past_week_hourly.append(append_time_parameters3.hour)
 
         #Machine Learning Aspect
 
-        clf = RandomForestRegressor(n_estimators=500, oob_score=True, random_state=100) #Define Predictor
+        #Define Predictor
+
+        clf = RandomForestRegressor(n_estimators=500, oob_score=True, random_state=100) 
         
-        df1_X = pd.DataFrame({'Month': month1, 'Hour': day1, 'Charger Type': charger_type1})
-        df1_y = pd.DataFrame({'Aggregated Amount': aggregated_amount_segment1, 'Total Power': total_power_segment1})
+        #Dataframe past month features
 
-        df2_X = pd.DataFrame({'Month': month2, 'Hour': day2, 'Charger Type': charger_type2})
-        df2_y = pd.DataFrame({'Aggregated Amount': aggregated_amount_segment2, 'Total Power': total_power_segment2})
+        df_past_month_X = pd.DataFrame({'Day': day_past_month, 'Charger Type': charger_type_past_month})
+        df_past_month_y = pd.DataFrame({'Aggregated Amount': aggregated_amount_past_month, 'Total Power': total_power_past_month})
 
-        df3_X = pd.DataFrame({'Month': month3, 'Hour': day3, 'Charger Type': charger_type3})
-        df3_y = pd.DataFrame({'Aggregated Amount': aggregated_amount_segment3, 'Total Power': total_power_segment3})
+        #Dataframe past year features
 
-        test_hour= []
+        df_past_year_X = pd.DataFrame({'Month': month_past_year, 'Charger Type': charger_type_past_year})
+        df_past_year_y = pd.DataFrame({'Aggregated Amount': aggregated_amount_past_year, 'Total Power': total_power_past_year})
+
+        #Dataframe past week hourly features
+
+        df_past_week_hourly_X = pd.DataFrame({'Hour': hour_past_week_hourly, 'Charger Type': charger_type_past_week_hourly})
+        df_past_week_hourly_y = pd.DataFrame({'Aggregated Amount': aggregated_amount_past_week_hourly, 'Total Power': total_power_past_week_hourly})
+
+        
+
+        #Append segments of time (day,month,hour) for test parameters
+        test_day= []
         test_month = []
+        test_hour = []
 
         for value in list_timestamps:
-            time1 = dateutil.parser.parse(value)
-            test_hour.append(time1.hour)
-            test_month.append(time1.month)
+            time_from_list = dateutil.parser.parse(value)
+            test_day.append(time_from_list.day)
+            test_month.append(time_from_list.month)
+            test_hour.append(time_from_list.hour)
 
+        
         #Define input charger type feature as a list for dataframe
 
         ev_charger_type_input_lvl2 = []
@@ -143,25 +147,54 @@ def predict_ev_init(sio):
             k = k + 1
 
 
-        #Create input dataframes
-        df_input_lvl2 = pd.DataFrame({'Month': test_month, 'Hour': test_hour, 'Charger Type': ev_charger_type_input_lvl2})
-        df_input_lvl3 = pd.DataFrame({'Month': test_month, 'Hour': test_hour, 'Charger Type': ev_charger_type_input_lvl3})
+        #Create input dataframes for lvl 2 and lvl 3 chargers respectively
 
-        clf.fit(df1_X, df1_y)
-        clf.predict(df1_X)
-        output1 = clf.predict(df_input_lvl2)
-        output11 = clf.predict(df_input_lvl3)
+        #For past month
 
-        clf.fit(df2_X, df2_y)
-        clf.predict(df2_X)
-        output2 = clf.predict(df_input_lvl2)
-        output22 = clf.predict(df_input_lvl3)
+        df_input_lvl2_past_month = pd.DataFrame({'Day': test_day, 'Charger Type': ev_charger_type_input_lvl2})
+        df_input_lvl3_past_month = pd.DataFrame({'Day': test_day, 'Charger Type': ev_charger_type_input_lvl3})
 
-        clf.fit(df3_X, df3_y)
-        clf.predict(df3_X)
-        output3 = clf.predict(df_input_lvl2)
-        output33 = clf.predict(df_input_lvl3)
-        final_output = (1/3)*(output11 + output22 + output33)
+        #For past year
+
+        df_input_lvl2_past_year = pd.DataFrame({'Month': test_month, 'Charger Type': ev_charger_type_input_lvl2})
+        df_input_lvl3_past_year = pd.DataFrame({'Month': test_month, 'Charger Type': ev_charger_type_input_lvl3})
+
+        #For past week hourly
+
+        df_input_lvl2_past_week_hourly = pd.DataFrame({'Hour': test_hour, 'Charger Type': ev_charger_type_input_lvl2})
+        df_input_lvl3_past_week_hourly = pd.DataFrame({'Hour': test_hour, 'Charger Type': ev_charger_type_input_lvl3})
+
+        #Prediction application for past month
+
+        clf.fit(df_past_month_X, df_past_month_y)
+        output_past_month_lvl2 = (clf.predict(df_input_lvl2_past_month))/24
+        output_past_month_lvl3 = (clf.predict(df_input_lvl3_past_month))/24
+
+        #Prediction application for past year
+
+        clf.fit(df_past_year_X, df_past_year_y)
+        output_past_year_lvl2 = (clf.predict(df_input_lvl2_past_year))/30.5
+        output_past_year_lvl3 = (clf.predict(df_input_lvl3_past_year))/30.5
+
+        #Prediction application for past week hourly
+
+        clf.fit(df_past_week_hourly_X, df_past_week_hourly_y)
+
+        output_past_week_hourly_lvl2 = clf.predict(df_input_lvl2_past_week_hourly)
+        output_past_week_hourly_lvl3 = clf.predict(df_input_lvl3_past_week_hourly)
+
+        #Weights for conservative, aggressive predictions, and neutral predictions 
+
+        conservative_prediction = 0.6
+        neutral_prediction = 0.3
+        aggressive_prediction = 0.1
+
+        #Add lvl2 and lvl3 charger outputs
+      
+        sum_of_lvl2 = (1/3)*(output_past_month_lvl2 + output_past_year_lvl2 + output_past_week_hourly_lvl2)
+        sum_of_lvl3 = (1/3)*(output_past_month_lvl3 + output_past_year_lvl3 + output_past_week_hourly_lvl3)
+
+        final_output = sum_of_lvl2 + sum_of_lvl3
 
         class NumpyArrayEncoder(json.JSONEncoder):
             def default(self, obj):
